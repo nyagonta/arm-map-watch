6d392cc8ff6fb8d17a1bf52d903825e1a7e62725
nyagonta <nyagonta@gmail.com>
2017-03-03 01:38:22 +0900

    add new file

---
+++ get_size_list.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+
+def get_status(file):
+    with open(file) as fp:
+        return fp.readline()
+
+script_name = sys.argv[0]
+
+try:
+    infile = sys.argv[1]
+    with open(infile, 'r') as fin:
+        lines = fin.readlines()
+except IndexError:
+    print 'Usage: %s map-file out-file' % script_name
+    quit()
+except IOError:
+    print '"%s" cannot be opened.' % infile
+    quit()
+
+try:
+    outfile = sys.argv[2]
+    with open(outfile, "r") as fref:
+        csvReader = csv.reader(fref)
+        ref_header = list(csvReader)[0]
+except IndexError:
+    print 'Usage: %s map-file out-file' % script_name
+    quit()
+except IOError:
+    ref_header = []
+
+header = []
+size = []
+with open(outfile, "ab") as fout:
+    for line in lines:
+        if line.find("Execution Region") >= 0:
+            data = line.split()
+            header.append(data[2])
+            size.append(int(data[4].replace(',',''), 16))
+    csvWriter = csv.writer(fout)
+    if ref_header is None:
+        csvWriter.writerow(header)
+    elif ref_header != header:
+        print 'warning: map layout change!!'
+        print 'old: %s' % ref_header
+        print 'current: %s' % header
+        quit()
+    csvWriter.writerow(size)
+
